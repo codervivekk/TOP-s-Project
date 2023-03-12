27025d7bc60836bdbe055e083479cef251d0cc2e
Vivek <codervivekk@gmail.com>
2023-03-13 01:06:24 +0530

    initial commit

--- JS/Rock-Paper-Scissor/main.js
+++ JS/Rock-Paper-Scissor/main.js
@@ -1,21 +1,25 @@
-//The complete program has been written with a minor mistake where computer is returning only "rock"
+// The complete program has been written with a minor mistake where computer is returning only "rock"
 
-
+const button = document.getElementsByClassName("button");
 
 function getComputerChoice(){
-    const R="Rock";
-    const P="Paper";
-    const S="Scissors";
-    
-    return R;
+    const shapes=[
+        "rock",
+        "paper",
+        "scissors"
+    ]
+    const random = Math.floor(Math.random()*shapes.length)
+        
+    return shapes[random];
 }
+
 
 function playRound(computerSelection,playerSelection){
     if((computerSelection == "ROCK" && playerSelection == "PAPER") || (computerSelection == "PAPER" && playerSelection =="SCISSORS") || (computerSelection == "SCISSORS" && playerSelection == "ROCK")){
         return "Player";
     }
     else if((computerSelection == "ROCK" && playerSelection == "ROCK") || (computerSelection == "PAPER" && playerSelection == "PAPER" || (computerSelection == "SCISSORS" && playerSelection == "SCISSORS"))){
-        return "Draw";
+        return "draw";
     }
     else{
         return "Computer"
@@ -27,9 +31,23 @@ let player=0,computer=0,draw=0;
 for(let i=0;i<5;i++){
 
     console.log(`Round ${i+1}`);
+
     let computerSelection = getComputerChoice();
-    let playerSelection = prompt("Enter Your Choice(Rock/Paper/Scissors):");
+
+    console.log(computerSelection);
     computerSelection = computerSelection.toUpperCase();
+
+    for (i of button) {
+        i.addEventListener("click", function(e) {
+         var playerSelection = e.srcElement.value; 
+         console.log(playerSelection);
+        })
+        break;
+    };
+
+
+    
+    console.log(playerSelection);
     playerSelection = playerSelection.toUpperCase();
 
 
@@ -56,8 +74,3 @@ else if(computer>=3){
 else{
     alert("Game Drawn!");
 }
-
-
-
-
-
